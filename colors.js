5be16454905c741b1383241d13a9f726caac44ed
Mandeep Singh <mandeepsingh120498@gmail.com>
2026-03-25 16:37:26 +0530

    Explore third party package installation like nodemon, colors for demo and also learn about package.json manual creation.

---
+++ colors.js
@@ -0,0 +1,12 @@
+const color = require('colors');
+
+console.log('This is red'.red);
+console.log('This is green'.green);
+console.log('This is blue'.blue);
+console.log('This is yellow'.yellow);
+console.log('This is cyan'.cyan);
+console.log('This is magenta'.magenta);
+console.log('This is white'.white);
+console.log('This is gray'.gray);
+
+console.log(color.red('This is red again'));
